3389dc905c53c05c49042e8e41e1afa90cc907fb
Sergei Korolev <dromgard@yandex.ru>
2022-08-23 10:22:47 +0300

    feat: создан проект, добавлены фото

---
+++ README.md
@@ -0,0 +1,14 @@
+# **Project: "Sergio birthday boy".**
+
+## **Project description:**
+This is a birthday greeting card. The header displays the phrase "Happy Birthday!" in different languages. A random photo of the birthday person is displayed in the center of the page. Clicking on it will display the next random photo.
+
+## **Technologies used:**
+* Adaptive layout is used in the project, the site looks great on screens with large and small resolution.
+* Media queries are used for adaptive layout.
+* Flex used for the layout of blocks of the site.
+* Language changing and photo flipping functionality is implemented in JavaScript. 
+* Animation is used in dynamic gradient backgrounds.
+
+## **Посмотреть проект он-лайн:**
+* [Сheck out the project](https://dromgard.github.io/sergio-birthday-boy/)
---
+++ script/index.js
@@ -0,0 +1,104 @@
+// DATA
+// Массив поздравлений на разных языках.
+const congratulations = [
+  'Feliz cumpleaños, Sanya!',
+  'Buon compleanno, Sanya!',
+  'Happy birthday, Sanya!',
+  'Alles Gute zum Geburtstag, Sanya!',
+  '生日快乐，三亚!',
+  'З днем народження, Саню!',
+  'Joyeux anniversaire, Sanya!',
+  'С днем рождения, Сергей!'
+];
+
+// CONSTANTS
+// Выбираем элементы из DOM.
+const sergioCongratulation = document.querySelector('.congratulations__text');
+const serhioPhoto = document.querySelector('.photo__common');
+const buttonAutoplay = document.querySelector('.controls__button_type_autoplay');
+const buttonAudioPlay = document.querySelector('.controls__button_type_audio');
+const audioFile = document.querySelector('.audio');
+
+// Задаем переменную для перебора массива поздравлений "congratulations".
+let congratulationsIndex = 0;
+
+// Задаем переменную для остановки слайдшоу.
+let autoplayIntervalID;
+
+// ACTIONS
+// Возвращаем по очереди поздравление из массива.
+function returnCongratulation(array) {
+  if (congratulationsIndex < array.length) {
+    sergioCongratulation.textContent = array[congratulationsIndex];
+    congratulationsIndex += 1;
+  } else {
+    congratulationsIndex = 0;
+    sergioCongratulation.textContent = array[congratulationsIndex];
+    congratulationsIndex += 1;
+  }
+};
+
+// Запускаем возврат поздравления из массива с интервалом в 3 секунды.
+setInterval(returnCongratulation, 3000, congratulations);
+
+// Выдаем случайное число из переданного диапазона. 
+function randomInteger(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Проверяем запущено ли слайдшоу. Да - останавливаем и запускаем ручной беребор.
+// Нет - Только ручной перебор.
+function checkSlideShowRunning() {
+  if (buttonAutoplay.classList.contains('playing')) {
+    stopSlideShow();
+    makeSerhioRandom();
+  } else {
+    makeSerhioRandom();
+  }
+};
+
+// Выдаем случайную фотографию, используя случайное число для формирования имени файла с фото.
+function makeSerhioRandom() {
+  serhioPhoto.textContent = '';
+  serhioPhoto.style.backgroundImage = `url('images/photo-${randomInteger(1, 66)}.jpg')`;
+};
+
+// Останавливаем слайдшоу из случайных фотографий
+function stopSlideShow() {
+  clearInterval(autoplayIntervalID);
+  buttonAutoplay.classList.remove('playing');
+  buttonAutoplay.textContent = 'Autoplay';
+};
+
+// Запускаем слайдшоу из случайных фотографий.
+function toggleSlideShow() {
+  if (buttonAutoplay.classList.contains('playing')) {
+    stopSlideShow();
+    serhioPhoto.textContent = 'Press me:)';
+  } else {
+    makeSerhioRandom();
+    const intervalID = setInterval(makeSerhioRandom, 2000);
+    buttonAutoplay.classList.add('playing');
+    buttonAutoplay.textContent = 'Stop Autoplay';
+    autoplayIntervalID = intervalID;
+  }
+};
+
+// Вставляем в DOM аудио трек, запускаем его, скрываем кнопку включения аудио.
+function playAudio() {
+  audioFile.innerHTML = `<audio controls loop src="audio/allegrova-happy-birthday.mp3" class="controls__audio" autoplay></audio>`;
+  audioFile.classList.remove('audio_hidden');
+  buttonAudioPlay.classList.add('controls__button_audio_hidden');
+};
+
+// LISTENERS
+// Кнопка ручного переключения фотографий.
+serhioPhoto.addEventListener('click', checkSlideShowRunning);
+
+// Кнопка автоматического переключения фотографий.
+buttonAutoplay.addEventListener('click', toggleSlideShow);
+
+// Кнопка включения аудио.
+buttonAudioPlay.addEventListener('click', playAudio);
